3babab11801bdd23df048153559f346d93f7deab
Xhameel Spencer <xhameelspencer@gmail.com>
2023-08-02 10:24:24 -0600

    Understanding the basics of S2T

--- Scripts/APIsPractice_TextToSpeech.py
+++ Scripts/APIsPractice_TextToSpeech.py
@@ -1,18 +1,31 @@
 from ibm_watson import SpeechToTextV1
 from dotenv import load_dotenv
+import json
 import os
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-def config():
-    load_dotenv()
-    authenticator_xs = IAMAuthenticator(os.getenv('IAM_api_key'))
-    speech_to_text = SpeechToTextV1(
-        authenticator=authenticator_xs
+load_dotenv()
+authenticator_xs = IAMAuthenticator(os.getenv('IAM_api_key'))
+speech_to_text = SpeechToTextV1(
+    authenticator=authenticator_xs
     )
-    speech_to_text.set_service_url(os.getenv('url_S2T'))
+speech_to_text.set_service_url(os.getenv('url_S2T'))
+
+"""This a project to understand the basics of the Speech to text API"""
 
 
-config()
+with open('C:/Users/Dell Latitud E7450/PycharmProjects/API/Testdata/Bob Marley  redemption song (Vocals Only).mp3','rb') as audio_file:
+    speech_results = speech_to_text.recognize(
+        audio= audio_file,
+        content_type= 'audio/mp3',
+    ).get_result()
 
+print(json.dumps(speech_results, indent=2))
 
-s2t = SpeechToTextV1(iam_apikey=os.getenv('api_key_S2T'), url= os.getenv('url_S2T'))
+transcripted = speech_results['results'][0]['alternatives'][0]['transcript']
+
+print(transcripted)
+
+#transcripted_file = open('C:/Users/Dell Latitud E7450/PycharmProjects/API/Transcripted data/file1.txt','w+')
+#transcripted_file.writelines(transcripted)
+#transcripted_file.close()
